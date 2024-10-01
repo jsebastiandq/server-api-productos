19c50c5ec51205edccf5a5aab1d99d6a412a2d18
J. Sebastian Duque <info@jsebastiandq.com>
2024-10-01 16:45:28 -0500

    Complete CRUD Clients

--- controllers/clienteController.js
+++ controllers/clienteController.js
@@ -12,5 +12,55 @@ exports.nuevoCliente = async (req, res) => {
     } catch(error) {
         // Si hay un error, console.log y next
         console.log(error);
+        next();
+    }
+}
+
+// Muestra todos los clientes
+exports.mostrarClientes = async (req, res, next) => {
+    try {
+        const clientes = await Clientes.find({});
+        res.json(clientes);
+    } catch(error) {
+        // Si hay un error, console.log y next
+        console.log(error);
+        next();
+    }
+}
+
+// Muestra un cliente por su ID
+exports.mostrarCliente = async(req, res, next) => {
+    const cliente = await Clientes.findById(req.params.idCliente);
+
+    if(!cliente) {
+        res.json({mensaje: "Ese cliente no existe!"});
+        next();
+    } 
+    // Mostrar cliente
+    res.json(cliente);
+    
+}
+
+// Actualiza un cliente por su (ID)
+exports.actualizarCliente = async(req, res, next) => {
+    try {
+        const cliente = await Clientes.findOneAndUpdate({_id : req.params.idCliente}, req.body , {
+            new: true
+        });
+        res.json(cliente)
+    } catch (error) {
+        console.log(error);
+        next();
+    }
+}
+
+// Eliminar cliente por su ID
+exports.eliminarCliente = async(req, res, next) => {
+    try {
+        await Clientes.findByIdAndDelete({_id : req.params.idCliente });
+        res.json({mensaje : "Cliente ha sido eliminado"});
+    } catch (error) {
+        console.log(error);
+        next();
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -17,5 +17,18 @@ module.exports = function() {
     // Agrega nuevos clientes via POST
     router.post('/clientes', clienteController.nuevoCliente)
 
+    // Obtener todos los clientes
+    router.get('/clientes', clienteController.mostrarClientes);
+
+    // Muestra un cliente en especifico (ID)
+    router.get('/clientes/:idCliente', clienteController.mostrarCliente);
+ 
+    // Actualizar Cliente
+    router.put('/clientes/:idCliente', clienteController.actualizarCliente);
+
+    // Eliminar Cliente
+    router.delete('/clientes/:idCliente', clienteController.eliminarCliente)
+
+
     return router;
 }
